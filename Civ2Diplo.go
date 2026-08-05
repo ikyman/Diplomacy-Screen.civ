ef4a8df5792c956b1750d89c6aec9c8d2217beac
Svet <70716489+ikyman@users.noreply.github.com>
2026-08-05 18:32:54 -0400

    Hello World: Project is in Go-Lang.
    Now for some speculation "Codec"/"Video Driver-esque thing" to use.
    I was tempted by TurboRes: https://github.com/Vanilagy/turbores .
    Allegedly, It's faster than ffmpeg. https://bsky.app/profile/vanilagy.bsky.social/post/3mrkuez3zlc27
    Ffmpeg is also the Boring & Mainstream Choice! However, I ultimately decided to opt for ffmpeg for Two Reasons:
    1. Documentation/Paper Trails would be easier for ffmpeg.
    2. I'm primarily interested in .mp4s, while TurboRes focuses on Apple's ProRes Formula. TurboRes DOES seem to be heavily Apple-Based. It's written in Typescript & Zig, though; Presumibally, it would work anywhere as long as someone gets their hands on an ProRes File.

---
+++ Civ2Diplo.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+    "fmt"
+
+)
+
+
+func main(){
+	fmt.Println("Golang Set Up! ")
+	fmt.Println("My \"Video Driver\" Options seem to be: ")
+	fmt.Println("Vanilagy: Turbores. This is heavily Apple-based.")
+    fmt.Println("It is written in Typescript & Zig, so it Shouuuld work anywhere. Right?")
+    fmt.Println("That being said, the example webpage doesn't take mp4. https://turbores.pages.dev/")
+    fmt.Println("ffmpeg: Reliable, & Ubiquitious. Lacks the Rebel Flair, though.")
+    
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Civ2Diplo
+
+go 1.24.5
